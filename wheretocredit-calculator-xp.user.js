7569d2acd5c415e80908e9fa677086f5816e2b94
Adam Hwang <adam.hwang@gmail.com>
2017-11-27 21:23:51 -0600

    Enabled paging and sorting

--- wheretocredit-calculator-xp.user.js
+++ wheretocredit-calculator-xp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wheretocredit.com calculator
 // @namespace    https://github.com/adamhwang/wheretocredit-calculator-xp
-// @version      1.1.3
+// @version      1.1.4
 // @description  Displays the number of frequent flyer miles you can earn with Expedia, Orbitz, Travelocity, Hotwire, Cheaptickets, Hotels.com, Wotif, ebookers, MrJet and SNCF! (all unaffiliated)
 // @author       wheretocredit.com
 // @include      http*://*.expedia.*/Flights-Search*
@@ -32,80 +32,81 @@
 // ==/UserScript==
 
 var main = function () {
+    var calc;
     getData (function(data, offers) {
-        $.ajax('//www.wheretocredit.com/api/2.0/calculate', {
+        calc = calc || $.ajax('//www.wheretocredit.com/api/2.0/calculate', {
             type : 'POST',
             contentType : 'application/json',
             dataType: 'json',
             data : JSON.stringify(data),
-            success: function (results) {
-                if (results.success) {
-                    var ota = $('#header-logo img').attr('alt') || $.grep(window.location.hostname.split('.'), function (n, i) { return i > 0; }).join('.').replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
-                    
-                    (function asyncLoop (i) {
-                        if (i < results.value.length) {
-                            
-                            var result = results.value[i];
-                            if (result.success && result.value.totals && result.value.totals.length) {
-                                // filter results
-                                result.value.totals = $.grep(result.value.totals, function (total) { return total.rdm[0] > 0; });
-                              
-                                if (result.value.totals.length) {
-                                    // order results
-                                    result.value.totals.sort(function (a, b) {
-                                        if (a.rdm[0] === b.rdm[0]) {
-                                            return +(a.name > b.name) || +(a.name === b.name) - 1;
+        });
+
+        calc.then(function (results) {
+            if (results.success) {
+                var ota = $('#header-logo img').attr('alt') || $.grep(window.location.hostname.split('.'), function (n, i) { return i > 0; }).join('.').replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+
+                (function asyncLoop (i) {
+                    if (i < results.value.length) {
+
+                        var result = results.value[i];
+                        if (result.success && result.value.totals && result.value.totals.length) {
+                            // filter results
+                            result.value.totals = $.grep(result.value.totals, function (total) { return total.rdm[0] > 0; });
+
+                            if (result.value.totals.length) {
+                                // order results
+                                result.value.totals.sort(function (a, b) {
+                                    if (a.rdm[0] === b.rdm[0]) {
+                                        return +(a.name > b.name) || +(a.name === b.name) - 1;
+                                    }
+                                    return b.rdm[0] - a.rdm[0]; // desc
+                                });
+
+                                var container = $(document.getElementById('flight-module-' + result.value.id.replace(/;/g, '_')));
+                                if (container.length) {
+                                    var height = container.height();
+
+                                    var addDisclaimer = function (carrier) {
+                                        if (carrier == 'UA' || carrier == 'DL' || carrier == 'AA') {
+                                            return '<span title="Revenue-based earning is overestimated because taxes are included in the base fare." style="color: #f00; cursor: help;">*</span>';
                                         }
-                                        return b.rdm[0] - a.rdm[0]; // desc
-                                    });
+                                        return '';
+                                    };
 
-                                    var container = $(document.getElementById('flight-module-' + result.value.id.replace(/;/g, '_')));
-                                    if (container.length) {
-                                        var height = container.height();
+                                    var html = '<div class="wheretocredit-wrap">' +
+                                        '<div class="wheretocredit-container" style="height: ' + (height-1-20) + 'px;">' +
+                                        result.value.totals.map(function (seg, i) { return '<div class="wheretocredit-item">' + seg.rdm[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + addDisclaimer(seg.id) + ' ' + seg.name + ' miles</div>'; }).join('') +
+                                        '</div>' +
+                                        '</div>';
 
-                                        var addDisclaimer = function (carrier) {
-                                            if (carrier == 'UA' || carrier == 'DL' || carrier == 'AA') {
-                                                return '<span title="Revenue-based earning is overestimated because taxes are included in the base fare." style="color: #f00; cursor: help;">*</span>';
-                                            }
-                                            return '';
-                                        };
+                                    container.before(html);
 
-                                        var html = '<div class="wheretocredit-wrap">' +
-                                            '<div class="wheretocredit-container" style="height: ' + (height-1-20) + 'px;">' +
-                                            result.value.totals.map(function (seg, i) { return '<div class="wheretocredit-item">' + seg.rdm[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + addDisclaimer(seg.id) + ' ' + seg.name + ' miles</div>'; }).join('') +
-                                            '</div>' +
-                                            '</div>';
-
-                                        container.before(html);
-
-                                        // ga event
-                                        var btn = container.find('button');
-                                        if (btn.length) {
-                                            var price = getOfferPrice(offers[result.value.id]);
-                                            var value = (price - bestPrice) / bestPrice;
-                                            value = Math.round(value * 1000); // ga only tracks ints
-                                            btn.click(function () {
-                                                wtcga('wtc.send', 'event', 'Trips', 'Select', ota, value);
-                                            });
-                                        }
+                                    // ga event
+                                    var btn = container.find('button');
+                                    if (btn.length) {
+                                        var price = getOfferPrice(offers[result.value.id]);
+                                        var value = (price - bestPrice) / bestPrice;
+                                        value = Math.round(value * 1000); // ga only tracks ints
+                                        btn.click(function () {
+                                            wtcga('wtc.send', 'event', 'Trips', 'Select', ota, value);
+                                        });
                                     }
                                 }
                             }
-                            
-                            setTimeout(function() { asyncLoop(i+1); }, 0);
                         }
-                        else {
-                            var disclaim = $('<div class="wheretocredit-disclaimer">Data provided by <a href="http://www.wheretocredit.com" target="_blank">wheretocredit.com</a> and is not affiliated or sponsored by ' + ota + '.  Your mileage may vary.</div>');
-                            disclaim.prependTo('.wheretocredit-wrap:first');
-                            disclaim.css('top', -1 * disclaim.height() - 20 + 'px');
-                        }
-                    })(0);
-                }
+
+                        setTimeout(function() { asyncLoop(i+1); }, 0);
+                    }
+                    else {
+                        var disclaim = $('<div class="wheretocredit-disclaimer">Data provided by <a href="http://www.wheretocredit.com" target="_blank">wheretocredit.com</a> and is not affiliated or sponsored by ' + ota + '.  Your mileage may vary.</div>');
+                        disclaim.prependTo('.wheretocredit-wrap:first');
+                        disclaim.css('top', -1 * disclaim.height() - 20 + 'px');
+                    }
+                })(0);
             }
         });
         
         injectCss();
-        
         
         var getOfferPrice = function (offer) { return offer.price.exactPrice; };
         var bestPrice = Math.min.apply(null, $.map(offers, getOfferPrice));
@@ -127,7 +128,7 @@ var main = function () {
                 });
             };
             
-            uitk.subscribe('Flights.ModuleBuilder.Controller.renderComplete', function() {
+            flights.vent.on('uiModel.resetViewableOffers', function() {
                 require(['uiModel'], function (uiModel) {
                     var data = $.map(uiModel.rawData.offers, function (offer, natrualKey) {
                         if (flights && flights.collections && flights.legsCollection)
